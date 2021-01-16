8c0da5320252a2c6ffcc4e0ba7f5dbe8679c79ff
Alex Breskin <alexdbreskin@gmail.com>
2021-01-16 22:05:40 +1100

    ✨ Declarative strategy implemented for lists

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,7 +1,6 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { EMPTY, Observable, of } from 'rxjs';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { EMPTY } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { Product } from './product';
 import { ProductService } from './product.service';
 
 @Component({
@@ -9,24 +8,20 @@ import { ProductService } from './product.service';
   styleUrls: ['./product-list.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
 
-  products$: Observable<Product[]>;
+  products$ = this.productService.products$
+  .pipe(
+    catchError(err => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  );
 
   constructor(private productService: ProductService) { }
-
-  ngOnInit(): void {
-    this.products$ = this.productService.getProducts()
-      .pipe(
-        catchError(err => {
-          this.errorMessage = err;
-          return EMPTY;
-        })
-      );
-  }
 
   onAdd(): void {
     console.log('Not yet implemented');
